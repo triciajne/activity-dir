09a8ed6653d4feebfdb2370b1212ee06355c8707
triciajne <triciajanetorres09@gmail.com>
2025-11-15 04:52:43 +0800

    Modified links inside htmls and code inside app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='template')
+
+@app.route('/')        
+def login_page():
+    return render_template('login.html')
+
+@app.route('/register')      
+def register_page():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
